653dcecce74736e986384f86b8947be26b424b44
Reginald Lamaute <rjlamaute1@gmail.com>
2017-07-20 12:10:55 -0400

    edited test calculator and calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+#this only does integers and rounds up because i'm trash like that
+def multiply(a,b):
+    return round(a * b, 3)
+def divide(a,b):
+    if b == 0:
+        return "you can't divide by zero you idiot"
+    else:
+        return round(a / float(b), 3)
--- test_calculator.py
+++ test_calculator.py
@@ -1,16 +1,36 @@
 from calculator import multiply
 from calculator import divide
 
-def test_numbers_2_4():
+def test_numbers_multiply_poz():
     assert multiply(2,4) == 8
-def test_numbers_4_2():
-    assert divide(4,2) == 2
+
+def test_numbers_multiply_neg():
+    assert multiply(-2,-4) == 8
+
+def test_numbers_multiply_neg_poz():
+    assert multiply(-2,4) == -8
 
 def test_numbers_0():
     assert multiply(0, 2) == 0
 
-def test_numbers_3_7():
-    assert divide(3,7) == 1
+def test_numbers_0():
+    assert multiply(0, 2) == 0
 
 def test_decimals():
-    assert (0.5, 0.2) == "error"
+    assert multiply (0.5, 0.2) == 0.1
+
+def test_numbers_divide_poz():
+    assert divide(4,2) == 2
+
+def test_numbers_divide_negative():
+    assert divide(-4, -2) == 2
+
+def test_numbers_divide_neg_poz():
+    assert divide(-4, 2) == -2
+
+def test_numbers_3_7():
+    assert divide(3,7) == 0.14
+
+def test_divideby0():
+    assert divide (2,0) == "error do not give me zero ever again"
+
